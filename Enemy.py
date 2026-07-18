f0e00a90ba6d405fe4e777d8ea3271456605f6c1
rvyso <rvysotski931@gmail.com>
2026-07-18 14:56:31 -0600

    1. Added animation 2. Added follow path function and will make enemy go to a point

--- Enemy.py
+++ Enemy.py
@@ -1,8 +1,11 @@
-import pygame
+import pygame, math
 
 class Enemy:
     IMAGE = pygame.transform.flip(pygame.transform.scale(pygame.image.load("assets/Zombie/Zombie_idle/Zombie_idle1.png"), (100, 100)), flip_x=1, flip_y=0)
-
+    zombie_animation = [pygame.image.load("assets/Zombie/Zombie_Walk/Zombie_Walk1.png"), pygame.image.load("assets/Zombie/Zombie_Walk/Zombie_Walk2.png"), pygame.image.load("assets/Zombie/Zombie_Walk/Zombie_Walk3.png"),
+                        pygame.image.load("assets/Zombie/Zombie_Walk/Zombie_Walk4.png"), pygame.image.load("assets/Zombie/Zombie_Walk/Zombie_Walk5.png"), pygame.image.load("assets/Zombie/Zombie_Walk/Zombie_Walk6.png"),
+                        pygame.image.load("assets/Zombie/Zombie_Walk/Zombie_Walk7.png"), pygame.image.load("assets/Zombie/Zombie_Walk/Zombie_Walk8.png"), pygame.image.load("assets/Zombie/Zombie_Walk/Zombie_Walk9.png"),
+                        pygame.image.load("assets/Zombie/Zombie_Walk/Zombie_Walk10.png")]
     def __init__(self, pos):
         self.image = Enemy.IMAGE
         self.rect = self.image.get_rect(center=pos)
@@ -12,14 +15,35 @@ class Enemy:
         self.gold_reward = 5
         self.isDead = True
         self.effects = []
-
-    def move(self, dt):
-        self.rect.x += -self.speed * dt
-        if self.rect.x <= 100 or self.rect.x >= 600:
-            self.speed = -self.speed
-            self.image = pygame.transform.flip(self.image, 1, 0)
+        self.animation_speed = 0.15
+        self.frame = 0
 
     def kill(self):
         if self.health <= 0:
             return self.isDead
         return False
+
+    def follow_path(self, path, dt):
+        px, py = path
+        dx = px - self.rect.x
+        dy = py - self.rect.y
+
+        distance = math.hypot(dx, dy)
+        dx /= distance
+        dy /= distance
+
+        if distance >= 30:
+            self.rect.x += dx * self.speed * dt
+            self.rect.y += dy * self.speed * dt
+            return None
+
+        return None
+
+    def animate_zombie(self):
+        self.frame += 0.3  # animation speed
+
+        if self.frame >= len(Enemy.zombie_animation):
+            self.frame = 0
+
+        self.image = pygame.transform.flip(pygame.transform.scale(Enemy.zombie_animation[int(self.frame)], (100, 100)), True, False)
+
