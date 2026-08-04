eaedec8d71c8fbc1e26b36899e9785e00dfcd478
rvyso <rvysotski931@gmail.com>
2026-08-03 20:29:48 -0600

    Added a fire cannon subclass that triggers fire effect

--- Cannons.py
+++ Cannons.py
@@ -3,26 +3,50 @@ import pygame
 cannons = {
     "Cannon" : {
         "damage" : 5,
-        "range" : 150,
+        "range" : 400,
         "reload" : 500,
-        "cost" : 5,
+        "bullet_size" : 5,
+        "projectile_speed" : 300,
+        "cost" : 0,
         "sprites" : {
             "up" : pygame.transform.scale(pygame.image.load("assets/Tower/Cannon/cannon_up.png"), (75, 75)),
             "down" : pygame.transform.scale(pygame.image.load("assets/Tower/Cannon/cannon_down.png"), (75, 75)),
             "left" : pygame.transform.scale(pygame.image.load("assets/Tower/Cannon/cannon_left.png"), (75, 75)),
-            "right" : pygame.transform.scale(pygame.image.load("assets/Tower/Cannon/cannon_right.png"), (75, 75))
-        }
+            "right" : pygame.transform.scale(pygame.image.load("assets/Tower/Cannon/cannon_right.png"), (75, 75)),
+            "bullet_image" : pygame.transform.scale(pygame.image.load("assets/Tower/Cannon/bullet.png"), (50,50))
+        },
+
     },
     "SniperCannon" : {
         "damage" : 15,
-        "range" : 300,
-        "reload" : 1000,
-        "cost" : 10,
+        "range" : 700,
+        "reload" : 1400,
+        "bullet_size" : 5,
+        "projectile_speed" : 350,
+        "cost" : 0,
         "sprites" : {
             "up" : pygame.transform.scale(pygame.image.load("assets/Tower/SniperCannon/sniper_cannon_up.png"), (80, 75)),
             "down" : pygame.transform.scale(pygame.image.load("assets/Tower/SniperCannon/sniper_cannon_down.png"), (80, 75)),
             "left" : pygame.transform.scale(pygame.image.load("assets/Tower/SniperCannon/sniper_cannon_left.png"), (80, 75)),
-            "right" : pygame.transform.scale(pygame.image.load("assets/Tower/SniperCannon/sniper_cannon_right.png"), (80, 75))
+            "right" : pygame.transform.scale(pygame.image.load("assets/Tower/SniperCannon/sniper_cannon_right.png"), (80, 75)),
+            "bullet_image" : pygame.transform.scale(pygame.image.load("assets/Tower/Cannon/bullet.png"), (40,40))
+        },
+    },
+    "FireCannon" : {
+        "damage" : 0,
+        "range" : 100,
+        "reload" : 700,
+        "projectile_speed" : 100,
+        "bullet_size" : 30,
+        "cost" : 0,
+        "fire_duration" : 3000,
+        "fire_dps" : 2,
+        "sprites" : {
+            "up" :pygame.transform.scale(pygame.image.load("assets/Tower/FireCannon/fire_cannon_up.png"), (100, 100)),
+            "down":pygame.transform.scale(pygame.image.load("assets/Tower/FireCannon/fire_cannon_up.png"), (100, 100)),
+            "left":pygame.transform.scale(pygame.image.load("assets/Tower/FireCannon/fire_cannon_up.png"), (100, 100)),
+            "right":pygame.transform.scale(pygame.image.load("assets/Tower/FireCannon/fire_cannon_up.png"), (100, 100)),
+            "bullet_image" : pygame.transform.scale(pygame.image.load("assets/Tower/FireCannon/fire_bullet.png"), (75, 75))
         }
     }
 }
