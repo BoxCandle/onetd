444afc27b62406ccbc203a856cf674a357b7c7a7
rvyso <rvysotski931@gmail.com>
2026-08-03 20:29:36 -0600

    Added a fire bullet subclass

--- Bullet.py
+++ Bullet.py
@@ -1,30 +1,52 @@
-import pygame
 import math
+from Effect import *
 
 class Bullet:
-    IMAGE = pygame.transform.scale(pygame.image.load("assets/Tower/bullet.png"), (40,40))
-
-    def __init__(self, pos, target, speed = 200, damage = 10, effect = None):
+    def __init__(self, pos, target, tower):
         self.x, self.y = pos
         self.target = target
-        self.speed = speed
-        self.damage = damage
-        self.effect = effect
-        self.image = self.IMAGE
+        self.speed = tower.projectile_speed
+        self.damage = tower.damage
+        self.size = tower.bullet_size
+        self.image = tower.sprites["bullet_image"]
         self.rect = self.image.get_rect(center=pos)
+        self.hitbox = self.rect.inflate(-35, -35)
+        self.hit_radius = (self.rect.width // 2) + 10
+        self.tower = tower
+
 
     def update(self, dt):
-        dx = self.target.rect.centerx - self.x
-        dy = self.target.rect.centery - self.y
+        dx = self.target.hitbox.centerx - self.x
+        dy = self.target.hitbox.centery - self.y
         distance = math.hypot(dx, dy)
-
-        if distance < 5: return self.deal_damage()
 
         if distance != 0:
             self.x += (dx / distance) * self.speed * dt
             self.y += (dy / distance) * self.speed * dt
             self.rect.center=(self.x, self.y)
-        return None
+            self.hitbox.center = self.rect.center
+
+        if self.hitbox.colliderect(self.target.hitbox):
+            self.deal_damage()
+            return True
+        return False
 
     def deal_damage(self):
         return self.damage
+
+class FireBullet(Bullet):
+    def __init__(self, pos, target, tower):
+        super().__init__(pos, target, tower)
+        self.image = tower.sprites["bullet_image"]
+        self.rect = self.image.get_rect(center=pos)
+        self.hitbox = self.rect.inflate(-65, -65)
+        self.hit_radius = max(self.rect.width, self.rect.height)
+        self.target = target
+
+    def update(self, dt):
+        hit = super().update(dt)
+
+        if hit:
+            self.tower.apply_effect(self.target)
+            return True
+        return False
